d3873251c7d0d1f32b63e0bc19a628981dec8966
fummicc1 <fumiya.tennis1@gmail.com>
2020-09-20 23:42:32 +0900

    Initial Commit

---
+++ SignInWithAppleUsingFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignInWithAppleUsingFirebase
+//
+//  Created by Fumiya Tanaka on 2020/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignInWithAppleUsingFirebase/SignInWithAppleUsingFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleUsingFirebaseApp.swift
+//  SignInWithAppleUsingFirebase
+//
+//  Created by Fumiya Tanaka on 2020/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleUsingFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
